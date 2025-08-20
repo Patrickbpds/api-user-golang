2aa51da53fd3b77726a4d0925e934c56183a0ea5
Patrick Batista <patrickbpds@gmail.com>
2025-08-20 17:42:41 -0300

    fix: correct formatting in UserRequest struct validation tags

--- src/controller/dtos/userRequest.go
+++ src/controller/dtos/userRequest.go
@@ -1,8 +1,8 @@
 package dtos
 
 type UserRequest struct {
-	Name string `json:"name" binding:"required, min=1,max=100"`
-	Password string `json:"password" binding:"required,min=8 containsany=@#$%&*"`
+	Name     string `json:"name" binding:"required,min=1,max=100"`
+	Password string `json:"password" binding:"required,min=8,containsany=@#$%&*"`
 	Email    string `json:"email" binding:"required,email"`
-	Age	  int8    `json:"age" binding:"required,min=0,max=120"`
+	Age      int8   `json:"age" binding:"required,min=0,max=120"`
 }

5cf17d176d00374fc01c91670d85ad73dab0e2b2
Patrick Batista <patrickbpds@gmail.com>
2025-08-20 15:10:45 -0300

    feat: initialize Gin router and set up routes in main function

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"os"
 
-	"github.com/lpernett/godotenv"
+  "github.com/lpernett/godotenv"
+  "github.com/gin-gonic/gin"
+  "api-user-golang/src/controller/routes"
 )
 
 func main() {
@@ -14,4 +16,11 @@ func main() {
     log.Fatal("Error loading .env file")
   } 
   fmt.Println(os.Getenv("TEST"))
+
+  router := gin.Default()
+
+  routes.InitRoutes(&router.RouterGroup)
+  if err := router.Run(":8080"); err != nil {
+    log.Fatalf("Failed to start server: %v", err)
+  }
 }

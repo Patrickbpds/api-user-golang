68c8c6d6626a35904f22960e9c9239b1a624084c
Patrick Batista <patrickbpds@gmail.com>
2025-08-20 16:18:07 -0300

    feat: update UserRequest struct to include validation rules and rename field

--- src/controller/dtos/userRequest.go
+++ src/controller/dtos/userRequest.go
@@ -1,8 +1,8 @@
 package dtos
 
 type UserRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	Age	  int8    `json:"age"`
+	Name string `json:"name" binding:"required, min=1,max=100"`
+	Password string `json:"password" binding:"required,min=8 containsany=@#$%&*"`
+	Email    string `json:"email" binding:"required,email"`
+	Age	  int8    `json:"age" binding:"required,min=0,max=120"`
 }

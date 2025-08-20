d8022ed6522af95fe9eb8d3b15679119878fe77c
Patrick Batista <patrickbpds@gmail.com>
2025-08-20 15:43:50 -0300

    feat: implement user creation logic with JSON binding and error handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,8 +1,20 @@
 package controller
 
 import (
+	rest_errors "api-user-golang/src/configuration/rest-errors"
+	"api-user-golang/src/controller/dtos"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser (c *gin.Context) {
+	var userRequest dtos.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_errors.NewBadRequestError(
+			fmt.Sprintf("There are invalid fields in the request body, error=%s\n", err.Error()))
+		c.JSON(restErr.Status, restErr)
+	}
+	fmt.Println("User request received: ", userRequest)
 }

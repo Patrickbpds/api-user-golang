d73f652c06504e5c514a3a983a9f3a9822fd5b2f
Patrick Batista <patrickbpds@gmail.com>
2025-08-20 12:11:53 -0300

    feat: define user routes with corresponding controller functions

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"api-user-golang/src/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUsersById/:userId")
-	r.GET("/getUsersByEmail/:userEmail")
-	r.GET("/getAllUsers")
-	r.POST("/createUser")
-	r.PUT("/updateUser/:userId")
-	r.DELETE("/deleteUser/:userId")
+	r.GET("/getUsersById/:userId", controller.FindUserById)
+	r.GET("/getUsersByEmail/:userEmail", controller.FindUserByEmail)
+	r.GET("/getAllUsers", controller.GetAllUsers)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 
 }

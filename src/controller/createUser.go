7f521d903def8ca286c0f0e60934fcd32addbbbf
Patrick Batista <patrickbpds@gmail.com>
2025-08-21 10:20:04 -0300

    feat: improve logging in CreateUser function for better error tracking

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,29 +1,37 @@
 package controller
 
 import (
+	"api-user-golang/src/configuration/logger"
 	"api-user-golang/src/configuration/validation"
 	"api-user-golang/src/controller/dtos"
-	"fmt"
-	"log"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Initializing CreateUser controller",
+		zap.String("jornal_action", "create_user"),
+	)
 	var userRequest dtos.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("There are invalid fields in the request body, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user request: %v", err,
+			zap.String("jornal_action", "create_user"),
+		)
 		errRest := validation.ValidateUserError(err)
 		c.JSON(errRest.Status, errRest)
 		return
 	}
-	fmt.Printf("UserRequest: %+v\n", userRequest)
+
 	response := dtos.UserResponse{
 		Id:    "12345",
 		Name:  userRequest.Name,
 		Email: userRequest.Email,
 		Age:   userRequest.Age,
 	}
+	logger.Info("User created successfully",
+		zap.String("jornal_action", "create_user"),
+	)
 	c.JSON(200, response)
 }

8c96732d12f494c7105ed63ebc0e59b105445f9d
Patrick Batista <patrickbpds@gmail.com>
2025-08-22 18:28:47 -0300

    refactor: clean up main function and improve dependency injection

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
 
 	"api-user-golang/src/configuration/database/mongodb"
 	"api-user-golang/src/configuration/logger"
-	"api-user-golang/src/controller"
 	"api-user-golang/src/controller/routes"
-	"api-user-golang/src/model/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lpernett/godotenv"
@@ -18,27 +14,24 @@ import (
 
 func main() {
 	logger.Info("Starting API User Service...")
+	
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	fmt.Println(os.Getenv("TEST"))
 
-	ctx := context.Background()
-	db, err := mongodb.NewMongoDbConnection(ctx)
+	database, err := mongodb.NewMongoDbConnection(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		log.Fatalf(
+			"Error trying to connect to database, error=%s \n",
+			err.Error())
+		return
 	}
 
-  _ = db
-
-	service := service.NewUserDomainService()
-	userController := controller.NewUserControllerInterface(service)
+	userController := initDependencies(database)
 
 	router := gin.Default()
-
+	gin.SetMode(gin.ReleaseMode)
 	routes.InitRoutes(&router.RouterGroup, userController)
+
 	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Fatal(err)
 	}
 }

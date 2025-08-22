541cc8cd31e0cdbd404b4b1d7e7ed7b77a4c7841
Patrick Batista <patrickbpds@gmail.com>
2025-08-22 17:44:42 -0300

    feat: add ID field, JSON tags and GetJSONValue method to UserDomain

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -3,6 +3,7 @@ package model
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 )
 
 type UserDomainInterface interface {
@@ -10,6 +11,9 @@ type UserDomainInterface interface {
 	GetPassword() string
 	GetName() string
 	GetAge() int8
+
+	SetId(id string)
+	GetJSONValue() (string, error)
 	EncryptPassword()
 }
 
@@ -18,34 +22,49 @@ func NewUserDomain(
 	age int8,
 	) UserDomainInterface {
 	return &userDomain{
-		email, password, name, age,}
+		Email:    email,
+		Password: password,
+		Name:     name,
+		Age:      age,
+	}
 }
 
-
+func (ud *userDomain)SetId(id string){
+	ud.ID = id
+}
 type userDomain struct {
-	email    string
-	password string
-	name     string
-	age      int8
+	ID    string `json:"id"`
+	Email    string `json:"email"`
+	Password string
+	Name     string
+	Age      int8
 }
+
+func (ud *userDomain) GetJSONValue() (string, error){
+	b, err := json.Marshal(ud)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+} 
 
 func (ud *userDomain) GetEmail() string{
-	return ud.email
+	return ud.Email
 }
 func (ud *userDomain) GetPassword() string {
-	return ud.password
+	return ud.Password
 }
 func (ud *userDomain) GetName() string {
-	return ud.name
+	return ud.Name
 }
 func (ud *userDomain) GetAge() int8 {
-	return ud.age
+	return ud.Age
 }
 
 
 func (ud *userDomain) EncryptPassword() {
 	hash := md5.New()
 	defer hash.Reset()
-	hash.Write([]byte(ud.password))
-	ud.password = hex.EncodeToString(hash.Sum(nil))
+	hash.Write([]byte(ud.Password))
+	ud.Password = hex.EncodeToString(hash.Sum(nil))
 }

4d5774f5c1dc94559d9943a438a7e7d175ceea5d
Patrick Batista <patrickbpds@gmail.com>
2025-08-26 11:50:36 -0300

    refactor: inject UserRepository and update CreateUser signature

--- src/model/service/user_interface.go
+++ src/model/service/user_interface.go
@@ -3,19 +3,22 @@ package service
 import (
 	rest_errors "api-user-golang/src/configuration/rest-errors"
 	"api-user-golang/src/model"
+	"api-user-golang/src/model/repository"
 )
 
 
-func NewUserDomainService() UserDomainService {
-	return &userDomainService{}
+func NewUserDomainService(userRepository repository.UserRepository,
+) UserDomainService {
+	return &userDomainService{userRepository}
 }
 
 type userDomainService struct {
+	userRepository repository.UserRepository
 
 }
 
 type UserDomainService interface {
-	CreateUser(model.UserDomainInterface) *rest_errors.RestError
+	CreateUser(model.UserDomainInterface) (model.UserDomainInterface, *rest_errors.RestError)
 	UpdateUser(string, model.UserDomainInterface) *rest_errors.RestError
 	GetUser(string) (*model.UserDomainInterface, *rest_errors.RestError)
 	DeleteUser(string) *rest_errors.RestError

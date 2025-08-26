21fece5e3df2f4edc76efe84cfa174d2374f26bc
Patrick Batista <patrickbpds@gmail.com>
2025-08-26 11:47:37 -0300

    feat: integrate CreateUser with repository layer

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -4,20 +4,20 @@ import (
 	"api-user-golang/src/configuration/logger"
 	rest_errors "api-user-golang/src/configuration/rest-errors"
 	"api-user-golang/src/model"
-	"fmt"
 
 	"go.uber.org/zap"
 )
 
 func (ud *userDomainService) CreateUser(
 	userDomain model.UserDomainInterface,
-) *rest_errors.RestError {
+) (model.UserDomainInterface, *rest_errors.RestError) {
 	
 	logger.Info("Init createUser model", zap.String("jornal_action", "create_user"))
 
 	userDomain.EncryptPassword()
 
-	fmt.Println("UserDomain:", userDomain.GetPassword())
+	userDomainRepository, err := ud.userRepository.CreateUser(userDomain)
+	if err != nil { return nil, err }
 
-	return nil
+	return userDomainRepository, nil
 }
